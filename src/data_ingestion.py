57a6cf4fc9ba11515733c7d527e7f506744e6bac
Kavya Sharma <404.kavya@gmail.com.git>
2026-07-18 05:34:41 +0530

    pipeline configured using dvc using dvc.yaml without params

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -93,7 +93,7 @@ def main():
         params = load_params(params_path = 'C:\\MLOps-ML-pipeline\\params.yaml')
         test_size = params['data_ingestion']['test_size']
         # test_size = 0.2
-        data_path = 'https://raw.githubusercontent.com/vikashishere/Datasets/main/spam.csv'
+        data_path = 'C:\\MLOps-ML-pipeline\\experiment\\spam.csv'
         df = load_data(data_url=data_path)
         final_df = preprocess_data(df)
         train_data, test_data = train_test_split(final_df, test_size=test_size, random_state=2)
